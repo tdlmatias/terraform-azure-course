42f8c15b2d5b3c415006afe3200a1bb573c856a5
tdlmatias <{ID}+{username}@users.noreply.github.com>
2022-03-17 22:53:43 +0000

    First success Build project

--- first-steps/instance.tf
+++ first-steps/instance.tf
@@ -6,11 +6,11 @@ resource "azurerm_virtual_machine" "demo-instance" {
   network_interface_ids = [azurerm_network_interface.demo-instance.id]
   vm_size               = "Standard_A1_v2"
 
-# For non Production we can allow terraform to delete all data on Termination
-  delete_os_disk_on_termination = true
+  # For non Production we can allow terraform to delete all data on Termination
+  delete_os_disk_on_termination    = true
   delete_data_disks_on_termination = true
 
-# define the OS type and version
+  # define the OS type and version
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
@@ -33,16 +33,16 @@ resource "azurerm_virtual_machine" "demo-instance" {
   os_profile_linux_config {
     disable_password_authentication = true
     ssh_keys {
-      key_data = file("mydemokey.pub")
+      key_data = file("~/.ssh/mydemokey.pub")
       path     = "/home/demo/.ssh/authorized_keys"
     }
   }
 }
 
 resource "azurerm_network_interface" "demo-instance" {
-  name                      = "${var.prefix}-instance1"
-  location                  = var.location
-  resource_group_name       = azurerm_resource_group.demo.name
+  name                = "${var.prefix}-instance1"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.demo.name
 
   ip_configuration {
     name                          = "instance1"
@@ -58,8 +58,8 @@ resource "azurerm_network_interface_security_group_association" "allow-ssh" {
 }
 
 resource "azurerm_public_ip" "demo-instance" {
-    name                         = "instance1-public-ip"
-    location                     = var.location
-    resource_group_name          = azurerm_resource_group.demo.name
-    allocation_method            = "Dynamic"
+  name                = "instance1-public-ip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.demo.name
+  allocation_method   = "Dynamic"
 }
